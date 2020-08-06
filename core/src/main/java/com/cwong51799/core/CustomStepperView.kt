e82fc8757d1ab1978a15ee5bee5b0d772b9e4359
Christopher Wong <cwong51799@gmail.com>
2020-08-05 21:42:27 -0400

    Linear Layout and Stack overflow is my hero. I think I just cracked the code on how to make semi-decent layouts. Thank you stack overflow.

--- core/src/main/java/com/cwong51799/core/CustomStepperView.kt
+++ core/src/main/java/com/cwong51799/core/CustomStepperView.kt
@@ -7,27 +7,35 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.custom_stepper_view.view.*
 
 class CustomStepperView @JvmOverloads constructor(
-    context : Context,
-    attrs : AttributeSet? = null
+    context: Context,
+    attrs: AttributeSet? = null
 ) : ConstraintLayout(context, attrs) {
 
-    init{
-        LayoutInflater.from(context).inflate(R.layout.custom_stepper_view,this, true)
+    init {
+        LayoutInflater.from(context).inflate(R.layout.custom_stepper_view, this, true)
         minusStep.setOnClickListener { decrementCount() }
         plusStep.setOnClickListener { incrementCount() }
     }
 
-
-    fun getCount() : Int{
+    fun getCount(): Int {
         return Integer.parseInt(stepperCount.text.toString())
     }
 
-    private fun incrementCount(){
-        val currentCount = Integer.parseInt(stepperCount.text.toString())
-        stepperCount.setText((currentCount+1).toString())
+    private fun incrementCount() {
+        val currentCount = getCount()
+        if (currentCount < MAX_QUESTION_COUNT)
+            stepperCount.text = (currentCount + 1).toString()
     }
-    private fun decrementCount(){
-        val currentCount = Integer.parseInt(stepperCount.text.toString())
-        stepperCount.setText((currentCount-1).toString())
+
+    private fun decrementCount() {
+        val currentCount = getCount()
+        if (currentCount > MIN_QUESTION_COUNT)
+            stepperCount.text = (currentCount - 1).toString()
+    }
+
+
+    companion object {
+        private const val MIN_QUESTION_COUNT = 1
+        private const val MAX_QUESTION_COUNT = 100
     }
 }
