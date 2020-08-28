8a9cf51c4c6ba0057b7128c1099de30c231365a8
Christopher Wong <cwong51799@gmail.com>
2020-08-27 21:11:52 -0400

    Added Glide for pictures, that's it for now. Just wanted to finish this up. time to work on school! To me, for next time I work on this. Do a better job error handling invalid IDs.

--- module-api/src/main/java/com/cwong51799/api/pokeapi/fragments/PokeAPIFragment.kt
+++ module-api/src/main/java/com/cwong51799/api/pokeapi/fragments/PokeAPIFragment.kt
@@ -1,14 +1,12 @@
 package com.cwong51799.api.pokeapi.fragments
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ScrollView
-import android.widget.TextView
+import android.widget.*
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -21,7 +19,11 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient
 import androidx.lifecycle.observe
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_poke_api.*
+import kotlinx.coroutines.supervisorScope
 import me.sargunvohra.lib.pokekotlin.model.Pokemon
+import java.lang.Exception
 
 /**
  * A simple [Fragment] subclass.
@@ -30,8 +32,12 @@ import me.sargunvohra.lib.pokekotlin.model.Pokemon
  */
 class PokeAPIFragment : Fragment() {
     private lateinit var viewModel: PokeAPIViewModel
-    private lateinit var navController : NavController
-    private lateinit var queryResult : TextView
+    private lateinit var navController: NavController
+    private lateinit var queryResult: TextView
+    private lateinit var pokemonIDSelector: EditText
+    private lateinit var pokemonSearchBtn: Button
+    private lateinit var pokemonNameTV: TextView
+    private lateinit var pokemonImageView : ImageView
     private val PokeApi = PokeApiClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,33 +54,46 @@ class PokeAPIFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val pokemonIDSelector = view.findViewById<EditText>(R.id.pokemonNumSelectorET)
-        val pokemonSearchBtn = view.findViewById<TextView>(R.id.pokemonSearchBtn)
-        viewModel.currentPokemon.observe(viewLifecycleOwner) {pokemon ->
+        pokemonIDSelector = view.findViewById<EditText>(R.id.pokemonNumSelectorET)
+        pokemonSearchBtn = view.findViewById<Button>(R.id.pokemonSearchBtn)
+        pokemonNameTV = view.findViewById<TextView>(R.id.pokemonNameTV)
+        pokemonImageView = view.findViewById<ImageView>(R.id.pokemonImageView)
+        viewModel.currentPokemon.observe(viewLifecycleOwner) { pokemon ->
             generatePokemonInformation(pokemon)
         }
-        pokemonSearchBtn.setOnClickListener{
-            searchAPokemon(Integer.parseInt(pokemonIDSelector.text.toString()))
+        pokemonSearchBtn.setOnClickListener {
+            if (isValidId(pokemonIDSelector.text.toString())) {
+                searchAPokemon(Integer.parseInt(pokemonIDSelector.text.toString()))
+            } else {
+                pokemonNameTV.text = POKEMON_ERROR_MSG
+                pokemonIDSelector.setText("1")
+            }
         }
         super.onViewCreated(view, savedInstanceState)
     }
 
+    private fun isValidId(id : String) : Boolean{
+        val idNum = Integer.parseInt(id)
+        return idNum in MIN_POKEMON_ID..MAX_POKEMON_ID
+    }
 
-    private fun searchAPokemon(id: Int){
-        GlobalScope.launch(Dispatchers.IO){
+    private fun searchAPokemon(id: Int) {
+        /* Currently not handling errors well. Such as when a
+         search for an ID which doesn't match any Pokemon. */
+        GlobalScope.launch {
             viewModel.setPokemon(PokeApi.getPokemon(id))
         }
     }
 
-    private fun generatePokemonInformation(pokemon : Pokemon) {
-
-
-
-
-
+    private fun generatePokemonInformation(pokemon: Pokemon) {
+        pokemonNameTV.text = pokemon.name.capitalize()
+        context?.let { Glide.with(it).load(pokemon.sprites.frontDefault).into(pokemonImageView) }
     }
 
     companion object {
         private val TAG = "PokeAPIFragment"
+        private val POKEMON_ERROR_MSG = "Error finding Pokemon data. Valid IDs range from 1-893."
+        private val MAX_POKEMON_ID = 893
+        private val MIN_POKEMON_ID = 1
     }
 }
