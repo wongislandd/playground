c2b0ed9f022db2d10ce29bd44cc3bdbec27fd994
Christopher Wong <46093907+cwong51799@users.noreply.github.com>
2021-05-28 19:38:16 -0400

    Added background types

--- module-api/src/main/java/com/cwong51799/api/pokeapi/fragments/DetailedPokemonFragment.kt
+++ module-api/src/main/java/com/cwong51799/api/pokeapi/fragments/DetailedPokemonFragment.kt
@@ -1,27 +1,28 @@
 package com.cwong51799.api.pokeapi.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.observe
-import com.bumptech.glide.Glide
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.cwong51799.api.R
 import com.cwong51799.api.pokeapi.custom_ui.HeightWeightView
-import com.cwong51799.api.pokeapi.custom_ui.SpriteView
+import com.cwong51799.api.pokeapi.sprite_recycler.SpriteRecyclerAdapter
 import com.cwong51799.api.pokeapi.viewmodels.PokeAPIViewModel
+import kotlinx.android.synthetic.main.detailed_pokemon_fragment.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient
 import me.sargunvohra.lib.pokekotlin.model.Pokemon
+import me.sargunvohra.lib.pokekotlin.model.PokemonSprites
 
 class DetailedPokemonFragment : Fragment() {
     private lateinit var viewModel: PokeAPIViewModel
+    private lateinit var spriteAdapter : SpriteRecyclerAdapter
     private val PokeApi = PokeApiClient()
 
     override fun onCreateView(
@@ -45,10 +46,18 @@ class DetailedPokemonFragment : Fragment() {
     private fun populatePokemonInfo(view: View, pokemon: Pokemon) {
         val heightWeightView = view.findViewById<HeightWeightView>(R.id.heightWeightView)
         val pokemonNameTV = view.findViewById<TextView>(R.id.pokemonNameTV)
-        val pokemonSpriteScrollView = view.findViewById<SpriteView>(R.id.custom_sprite_scroll_view)
-        pokemonSpriteScrollView.addSpritesToCarosel(pokemon.sprites)
+        initRecyclerView(pokemon.sprites)
         pokemonNameTV.text = pokemon.name.capitalize()
         heightWeightView.setHeightAndWeight(pokemon.height, pokemon.weight)
+    }
+
+    private fun initRecyclerView(sprites : PokemonSprites) {
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@DetailedPokemonFragment.context, LinearLayoutManager.HORIZONTAL, false)
+            spriteAdapter = SpriteRecyclerAdapter()
+            spriteAdapter.submitSprites(sprites)
+            adapter = spriteAdapter
+        }
     }
 
     companion object {
